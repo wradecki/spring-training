8e3cae57ae822d6c5b6ebdf765c9fb4f6ec93535
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-09-20 14:44:42 +0200

    Init

--- src/main/java/pl/training/shop/Application.java
+++ src/main/java/pl/training/shop/Application.java
@@ -1,7 +1,7 @@
 package pl.training.shop;
 
 import lombok.extern.java.Log;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pl.training.shop.payments.LocalMoney;
 import pl.training.shop.payments.PaymentRequest;
 import pl.training.shop.payments.PaymentService;
@@ -9,10 +9,10 @@ import pl.training.shop.payments.PaymentService;
 @Log
 public class Application {
 
-    private static final String BASE_PACKAGE = "pl.training.shop";
+    private static final String CONFIGURATION_FILE = "beans.xml";
 
     public static void main(String[] args) {
-        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(BASE_PACKAGE)) {
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(CONFIGURATION_FILE)) {
             var paymentService = context.getBean(PaymentService.class);
             var paymentRequest = PaymentRequest.builder()
                     .money(LocalMoney.of(1_000))
--- src/main/java/pl/training/shop/payments/LoggingProxy.java
+++ src/main/java/pl/training/shop/payments/LoggingProxy.java
@@ -2,17 +2,13 @@ package pl.training.shop.payments;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
 
-@Aspect
 @Log
 @RequiredArgsConstructor
 public class LoggingProxy {
 
     private static final String LOG_FORMAT = "A new payment of %s has been initiated";
 
-    @AfterReturning(value = "@annotation(LogPayments)", returning = "payment")
     public void log(Payment payment) {
         var entry = String.format(LOG_FORMAT, payment.getMoney());
         log.info(entry);
