232fe819780c1010471f9ea73b6134c0bf97184b
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-09-22 17:29:51 +0200

    Init

--- src/main/java/pl/training/shop/ShopConfiguration.java
+++ src/main/java/pl/training/shop/ShopConfiguration.java
@@ -2,6 +2,7 @@ package pl.training.shop;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pl.training.shop.payments.api.PaymentRepository;
@@ -16,6 +17,14 @@ public class ShopConfiguration implements WebMvcConfigurer {
     @Bean
     public PaymentService paymentService(PaymentRepository paymentRepository) {
         return paymentsServiceFactory.create(paymentRepository);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOrigins("http://localhost:4200");
     }
 
     @Override
